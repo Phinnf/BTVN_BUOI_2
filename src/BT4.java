a4e96775129d5b9bf81b046ad2e17b41096ac200
Phinnf <bnm915915@gmail.com>
2024-06-02 18:18:39 +0700

    my changes

--- src/BT4.java
+++
@@ -1,9 +0,0 @@
-public class BT4 {
-    public static void main(String[] args) {
-        TimeConservation phut = new TimeConservation();
-        int ConvertHourToMin = phut.ConvertHourToMin(2);
-        System.out.printf("So phut la: %d", ConvertHourToMin);
-        int ConvertDaysToMin = phut.ConvertDaysToMin(3);
-        System.out.printf("\nSo phut la: %d ", ConvertDaysToMin);
-    }
-}
